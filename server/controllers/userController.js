53c4186523c3159df847e41c69a53c871b4d6a0e
Mubasshir08 <msr4212019@gmail.com>
2025-01-11 16:18:59 +0600

    register and login logic done

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,12 +1,45 @@
 const mongoose = require('mongoose');
+
+// register
 const register = async (req,res) => {
     try {
         const {fullName, userName, password, confirmPassword, gender} = req.body
         if(!fullName || !userName || !password || !confirmPassword){
-            res.status(400).json({message : "All fields are required"})
+           return res.status(400).json({message : "All fields are required"})
         }
         if(fullName !== confirmPassword){
-            res.status(400).json({message : "Password do not match"})
+           return res.status(400).json({message : "Password do not match"})
+        }
+        const user = await User.findOne({userName})
+        if(user){
+            return res.status(400).json({message: "User Already Exist"})
+        }
+        const hashedPassword = await bcrypt.hash(password, 10)
+        
+        await User.create({
+            fullName,
+            userName,
+            password : hashedPassword,
+            gender
+        });
+
+        return res.status(201).json({
+            message: "Account Created Successfully",
+            success : true
+        })
+
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+// login
+const login = async (req,res) => {
+    try {
+        const {userName, password} = req.body
+        const userInfo = await User.findOne({userName})
+        if(!userInfo){
+            res.status(201).json({message: "Invalid username or password"})
         }
     } catch (error) {
         console.log(error)
