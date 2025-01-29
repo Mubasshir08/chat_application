efd6cda47276723d823d88a8a5c1020f8851d537
Mubasshir08 <msr4212019@gmail.com>
2025-01-29 13:03:38 +0600

    redux added

---
+++ client/src/redux/UserSlice.js
@@ -0,0 +1,16 @@
+import {createSlice} from '@reduxjs/toolkit';
+
+const UserSlice = createSlice({
+    name: 'user',
+    initialState:{
+        authUser: null
+    },
+    reducers:{
+        setAuthUser: (state, action) => {
+            state.authUser = action.payload;
+        }
+    }
+});
+
+export const {setAuthUser} = UserSlice.actions;
+export default UserSlice.reducer;
