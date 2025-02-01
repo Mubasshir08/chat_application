b896488e1c88327280b2a13cbd3aa499e03efbd3
Mubasshir08 <msr4212019@gmail.com>
2025-02-01 19:36:54 +0600

    scroll-bar design added for webkit browser

--- client/src/pages/Messages.jsx
+++ client/src/pages/Messages.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import Message from './Message'
 const Messages = () => {
   return (
-    <div className='pt-5 flex-1 overflow-auto'>
+    <div className='pt-5 px-2 flex-1 overflow-auto'>
       <Message receiver = "me"/>
       <Message receiver = "other"/>
       <Message receiver = "me"/>
