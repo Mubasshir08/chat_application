7e3000cddc5de091e4d67e9ec4d4eb5ee6d4af61
Mubasshir08 <msr4212019@gmail.com>
2025-02-01 23:35:39 +0600

    logout functionality added

--- client/src/pages/Sidebar.jsx
+++ client/src/pages/Sidebar.jsx
@@ -1,8 +1,23 @@
 import React from 'react';
 import { IoSearchCircle } from "react-icons/io5";
 import UsersDisplay from './UsersDisplay';
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
+import toast from 'react-hot-toast';
 
 const Sidebar = () => {
+  const navigate = useNavigate();
+  const logout = async () => {
+    try {
+      const response = await axios.get(`${import.meta.env.VITE_SERVER_URL}/user/logout`);
+      console.log(response.data)
+      if (response.status === 200) toast.success(response.data.message);
+      navigate('/login');
+    } catch (error) {
+        toast.error(error.response.data.message || 'An error occurred during logout.');
+        console.log(error)
+    }
+  }
   return (
     <div className='w-1/3 border-r-2 border-gray-700 overflow-x-auto flex flex-col'>
       <form className='sticky top-3 mt-5 text-center'>
@@ -13,7 +28,7 @@ const Sidebar = () => {
       </form>
       <div className="divider"></div>
         <UsersDisplay />
-        <button className='btn btn-error text-white rounded-none'>Logout</button>
+        <button className='btn btn-error text-white rounded-none' onClick={logout}>Logout</button>
     </div>
   )
 }
