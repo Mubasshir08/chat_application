d848f498caf27ac16ccb845f3bee04ca486b5758
Mubasshir08 <msr4212019@gmail.com>
2025-02-04 21:33:11 +0600

    message slice added

--- client/src/hooks/useGetOtherUsers.jsx
+++ client/src/hooks/useGetOtherUsers.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect } from 'react'
 import axios from 'axios'
 import { useDispatch } from 'react-redux'
-import { setOtherUsers } from '../redux/UserSlice';
+import { setOtherUsers } from '../redux/userSlice';
 
 const useGetOtherUsers = () => {
   const dispatch = useDispatch();
