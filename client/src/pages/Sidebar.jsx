d6c8bac843b9da9e29705ce419493c6fc3639853
Mubasshir08 <msr4212019@gmail.com>
2025-02-09 19:20:25 +0600

    search button position design fix

--- client/src/pages/Sidebar.jsx
+++ client/src/pages/Sidebar.jsx
@@ -11,13 +11,31 @@ const Sidebar = () => {
   const navigate = useNavigate();
   const {otherUsers} = useSelector(state => state.user)
   const [searchInput, setSearchInput] = useState('');
+  const [isUserFound, setIsUserFound] = useState(true)
   const [searchResult, setSearchResult] = useState([]);
   useEffect(() => {
-    const searchedUser = searchInput !== '' && searchInput !== " " && otherUsers?.filter(user => (user.userName.toLowerCase().includes(searchInput.toLowerCase())))
+    const searchedUser = searchInput !== " " && otherUsers?.filter(user => (user.userName.toLowerCase().includes(searchInput.toLowerCase())))
     setSearchResult(searchedUser);
     // console.log(searchedUser);
-  },[searchInput])
-  // console.log(searchInput)
+    if(searchResult && searchResult.length > 0){
+      setIsUserFound(true)
+    }
+   
+  },[searchInput, searchResult]);
+  console.log('searchResult: ',searchResult)
+  console.log('searchInput: ',searchInput)
+
+
+const searchBarHandler = (e) => {
+  e.preventDefault();
+  console.log('click')
+  if(!searchResult || searchResult.length === 0){
+    setIsUserFound(false)
+  }
+}
+
+console.log('isUserFound' ,isUserFound)
+
   const logoutHandler = async () => {
     try {
       const response = await axios.get(`${import.meta.env.VITE_SERVER_URL}/user/logout`);
@@ -32,14 +50,14 @@ const Sidebar = () => {
   }
   return (
     <div className='w-1/3 border-r-2 border-gray-700 overflow-x-auto flex flex-col'>
-      <form className='sticky top-3 mt-5 text-center' onSubmit={(e) => e.preventDefault()}>
-        <input type="text" className='input input-sm w-3/4' value={searchInput} onChange={(e) => setSearchInput(e.target.value)}/>
-        <button type='submit' className='absolute right-12 top-0.5'>
+      <form className='sticky top-3 mt-5 mx-3 text-center' onSubmit={(e) => searchBarHandler(e)}>
+        <input type="text" className='input input-sm w-full' value={searchInput} onChange={(e) => setSearchInput(e.target.value)}/>
+        <button type='submit' className='absolute inset-2/3 lg:inset-[85%] lg:top-[0.120rem] top-0.5'>
           <IoSearchCircle className='text-black' size="30px" />
         </button>
       </form>
       <div className="divider"></div>
-        <UsersDisplay searchedUser={searchResult}/>
+        {isUserFound ? <UsersDisplay searchedUser={searchResult}/> : <h3>Not Found</h3>}
         <button className='btn btn-error text-white rounded-none' onClick={logoutHandler}>Logout</button>
     </div>
   )
