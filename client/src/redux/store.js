73f790135eb98279322aa21ad791ffee08c373ed
Mubasshir08 <msr4212019@gmail.com>
2025-02-09 13:03:58 +0600

    redux state persisted

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,10 +1,36 @@
 import {configureStore} from '@reduxjs/toolkit';
+import {
+    persistStore,
+    persistReducer,
+    FLUSH,
+    REHYDRATE,
+    PAUSE,
+    PERSIST,
+    PURGE,
+    REGISTER,
+  } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 import userReducer from './userSlice';
 import messageReducer from './messageSlice';
 import rootReducer from './rootReducer';
 
+const persistConfig = {
+    key: 'root',
+    version: 1,
+    storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+
 const store = configureStore({
-    reducer: rootReducer,
+    reducer: persistedReducer,
+    middleware: (getDefaultMiddleware) =>
+        getDefaultMiddleware({
+          serializableCheck: {
+            ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+        },
+    })
 });
 
 export default store;
