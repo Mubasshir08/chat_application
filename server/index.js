208e1b7fbfe8b162156017012903126e95f3082e
Mubasshir08 <msr4212019@gmail.com>
2025-02-10 22:20:32 +0600

    test api

--- server/index.js
+++ server/index.js
@@ -16,8 +16,7 @@ const PORT = process.env.PORT || 3000;
 app.use(cors({
     origin: process.env.CLIENT_URL,
     credentials: true,
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    allowedHeaders: ["Content-Type", "Authorization"] 
+    methods: ["GET", "POST", "PUT", "DELETE"], 
 }));
 app.use(express.json());
 app.use(cookieParser());
