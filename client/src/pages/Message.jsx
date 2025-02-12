76a7ef2f025cf8b64531cef829de18de593e3d6e
Mubasshir08 <msr4212019@gmail.com>
2025-02-12 12:19:16 +0600

    real time showing update, second remove

--- client/src/pages/Message.jsx
+++ client/src/pages/Message.jsx
@@ -24,7 +24,6 @@ const Message = ({ messageInfo }) => {
       timeZone: "Asia/Dhaka",
       hour: "2-digit",
       minute: "2-digit",
-      second: "2-digit",
       hour12: true // 12-hour format with AM/PM
     };
     return new Intl.DateTimeFormat("en-US", options).format(date);
